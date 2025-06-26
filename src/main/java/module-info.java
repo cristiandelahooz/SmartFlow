a322045a5573bf2fe90a5050d0436bf543646ee6
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-06-25 23:40:13 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.trafficmanagement.smartflow {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.trafficmanagement.smartflow to javafx.fxml;
+    exports com.trafficmanagement.smartflow;
+}
